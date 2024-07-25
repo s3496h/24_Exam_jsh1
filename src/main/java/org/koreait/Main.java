e08fcfbae7999c9374a32ccc68a39acb03e7581b
ãs3496h <s3496h@gmail.com>
2024-07-25 17:09:27 +0900

    2번문제

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -6,12 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int m = in.nextInt();
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.print("*");
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 5-i; j++) {
+                System.out.print("a");
             }
-            System.out.println();
+            for (int j = 1; j <= (i*2)-1; j++) {
+            System.out.print("*");
         }
+        System.out.println(" ");
     }
+}
 }
